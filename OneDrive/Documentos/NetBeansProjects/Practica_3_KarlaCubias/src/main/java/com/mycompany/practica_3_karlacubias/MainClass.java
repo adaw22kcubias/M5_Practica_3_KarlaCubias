0dc0ca86e7d28f95a74a4c189d624dd45e2285a3
adaw22kcubias <adaw22kcubias@inslaferreria.cat>
2023-03-13 19:15:23 +0100

    Subiendo projecto de libreria

---
+++ OneDrive/Documentos/NetBeansProjects/Practica_3_KarlaCubias/src/main/java/com/mycompany/practica_3_karlacubias/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.practica_3_karlacubias;
+
+/**
+ *
+ * @author Karla Cubias
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+}
