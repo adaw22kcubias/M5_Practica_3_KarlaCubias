b7d832d7e459209d1a7a40ea9845cf8621888e4c
adaw22kcubias <adaw22kcubias@inslaferreria.cat>
2023-03-31 10:54:39 +0200

    Metodos acabados :)

--- OneDrive/Documentos/NetBeansProjects/Practica_3_KarlaCubias/src/main/java/com/mycompany/practica_3_karlacubias/MainClass.java
+++ OneDrive/Documentos/NetBeansProjects/Practica_3_KarlaCubias/src/main/java/com/mycompany/practica_3_karlacubias/MainClass.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.practica_3_karlacubias;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,8 +13,18 @@ package com.mycompany.practica_3_karlacubias;
 public class MainClass {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int dia, mes, any;
 
-       
-        
+        System.out.println("________________________________\n"
+                + "Introdueix la data del teu aniversari (dd/mm/aaaa)");
+        dia = in.nextInt();
+        mes = in.nextInt();
+        any = in.nextInt();
+        MyUtils.edat(dia, mes, any);
+        System.out.println("________________________________\n");
+
+        System.out.println(MyUtils.inverteix("hola como estas"));
+
     }
 }
--- OneDrive/Documentos/NetBeansProjects/Practica_3_KarlaCubias/src/main/java/com/mycompany/practica_3_karlacubias/MyUtils.java
+++ OneDrive/Documentos/NetBeansProjects/Practica_3_KarlaCubias/src/main/java/com/mycompany/practica_3_karlacubias/MyUtils.java
@@ -16,49 +16,122 @@ public class MyUtils {
      * @return cadena invertida (null per cadenes nulls).
      */
     public static String inverteix(String cadena) {
-        String resultat = "";
-        return resultat;
+        String texto = "";
 
-    }
+        if (cadena == null) {
 
-    /**
-     *
-     * @param day int dia del naixement
-     * @param month int mes del naixement
-     * @param year int any del naixement
-     * @return edat de la persona, per edat>150 retorna -1, per dates
-     * impossibles retorna -2
-     *
-     */
-    public static int edat(int day, int month, int year) {
-        int resultat = 0;
-       
-        
-        
-        
-        
-        
-        
-        
-        
-        return resultat;
-        
-    }
+            return null;
 
-    /**
-     *
-     * @param numero número del que es calcula el factorial
-     * @return retorna el factorial d'un número. Si el nombre es negatiu retorna
-     * -1.
-     */
-    public static double factorial(double numero) {
-
-        if (numero == 0) {
-            return 1;
         } else {
-            double resultat = numero * factorial(numero - 1);
-            return resultat;
+
+            for (int i = cadena.length() - 1; i >= 0; i--) {
+
+                texto += cadena.charAt(i);
+            }
+
+            return texto;
         }
     }
 
+    /**
+     *
+     * @param dayIn int dia del naixement
+     * @param mesIn int mes del naixement
+     * @param anyIn int any del naixement
+     * @return edat de la persona, per edat > 150 retorna -1, per dates
+     * impossibles retorna -2
+     *
+     */
+    public static int edat(int dayIn, int mesIn, int anyIn) {
+        int anyActual = 2023;
+        int anyImpossible = 150;
+       
+
+        //calcular edad
+        int edatCalculada = anyActual - anyIn;
+
+        // Detectar error en mesIn
+        if (mesIn < 1 || mesIn > 12) {
+
+            return -2;
+
+            // calcular si nombre de dies del mes es una data impossible:
+        } else if (comprobarDiesSegunMes(dayIn, mesIn, anyIn)) {
+
+            return -2;
+
+            // comprobar que no es una edat impossible
+        } else if (edatCalculada > anyImpossible || edatCalculada < 0) {
+
+            return -1;
+        }
+
+        return edatCalculada;
+
+    }
+
+    /**
+     *
+     * @param numRebut número del que es calcula el factorial
+     * @return retorna el factorial d'un número. Si el nombre es negatiu retorna
+     * -1.
+     */
+    public static double factorial(double numRebut) {
+
+        int factorial = 1;
+
+        if (numRebut < factorial) {
+
+            System.out.println("-1");
+            return -1;
+
+        } else {
+
+            for (int i = 1; i <= numRebut; i++) {
+                factorial *= i;
+            }
+            System.out.println(factorial);
+
+            return factorial;
+        }
+    }
+
+    /**
+     *
+     * @param dayIn int dia del naixement
+     * @param mesIn int mes del naixement
+     * @param anyIn int any del naixement
+     * @return boolean true si el mes correspon i false si és un mes impossible
+     *
+     */
+    public static boolean comprobarDiesSegunMes(int dayIn, int mesIn, int anyIn) {
+        int diesMes;
+
+        switch (mesIn) {
+            case 2:
+                /*Un any és traspas si:
+                    -. és múltiple de 4 () i no de 100, (com 1988, però no 1900)
+                    -. és múltiple de 4 i de 400 (com 2000)
+                 */
+                if (anyIn % 4 == 0 && (anyIn % 100 != 0 || anyIn % 400 == 0)) {
+                    diesMes = 29;
+
+                } else {
+                    diesMes = 28;
+                }
+                break;
+            case 11:
+                diesMes = 30; //abril(4) juny(6) setembre(9) novembre(11)
+                break;
+            default:
+                diesMes = 31; //la resta de dies que no tenen ni 28-29 ni 30 dies
+            }
+        // Detectar error al dia:
+        if (dayIn < 1 || dayIn > diesMes) {
+
+            return true;
+        }
+
+        return false; // <<--- no se porque no me devuelve esto
+    }
 }
